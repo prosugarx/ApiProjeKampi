94444c616aa2789070c2246bcefcadc2dc11828d
rumeysavarli23@gmail.com <rumeysavarli23@gmail.com>
2025-02-03 17:18:17 +0300

    Add project files.

---
+++ ApiProjeKampi.WebApi/Entities/Service.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiProjeKampi.WebApi.Entities
+{
+    public class Service
+    {
+        public int Serviceİd { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string IconUrl { get; set; }
+    }
+}
