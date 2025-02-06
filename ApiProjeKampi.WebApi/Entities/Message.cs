481872a6b3ff13cf4fc5d22c04e0f6d22553266f
rumeysavarli23@gmail.com <rumeysavarli23@gmail.com>
2025-02-07 01:32:00 +0300

    ApiProje

---
+++ ApiProjeKampi.WebApi/Entities/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiProjeKampi.WebApi.Entities
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string NameSurname { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string MesaageDetails { get; set; }
+        public DateTime SendTime { get; set; }
+        public bool Status { get; set; }
+    }
+}
--- ApiProjeKampi.WebApi/Entities/Service.cs
+++ ApiProjeKampi.WebApi/Entities/Service.cs
@@ -2,7 +2,7 @@
 {
     public class Service
     {
-        public int Serviceİd { get; set; }
+        public int ServiceId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string IconUrl { get; set; }
