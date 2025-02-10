8682be0d3bad2beef9b592986cdfe90f1822b3b5
rumeysavarli23@gmail.com <rumeysavarli23@gmail.com>
2025-02-10 20:52:39 +0300

    yummyDbswagger

---
+++ ApiProjeKampi.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using ApiProjeKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok("Kategori ekleme işlemi başarılı");
+        }
+    }
+}
